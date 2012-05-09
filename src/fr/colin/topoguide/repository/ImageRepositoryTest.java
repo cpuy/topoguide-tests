d1ba14535d6d0dbb55b0249d5c134f7c257aa83d
Colin PUY <colin.puy@gmail.com>
2012-05-09 13:33:32 +0200

    fix imageRepository issues

--- src/fr/colin/topoguide/repository/ImageRepositoryTest.java
+++ src/fr/colin/topoguide/repository/ImageRepositoryTest.java
@@ -1,25 +1,24 @@
 package fr.colin.topoguide.repository;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.test.AndroidTestCase;
-import fr.colin.topoguide.utils.IOUtils;
+import android.graphics.BitmapFactory;
+import android.test.InstrumentationTestCase;
 import fr.colin.topoguide.views.test.R;
 
-public class ImageRepositoryTest extends AndroidTestCase {
+public class ImageRepositoryTest extends InstrumentationTestCase {
 
    private ImageRepository imageRepository;
 
    @Override
    protected void setUp() throws Exception {
-      imageRepository = new ImageRepository(getContext());
+      imageRepository = new ImageRepository(getInstrumentation().getContext());
    }
 
    @Override
@@ -27,30 +26,54 @@ public class ImageRepositoryTest extends AndroidTestCase {
       FileUtils.deleteDirectory(imageRepository.getBaseFolder());
    }
    
-   private void addImageToRepository(long topoId, long imageId) throws RepositoryException, IOException {
-      InputStream in = getContext().getResources().openRawResource((int) imageId);
-      imageRepository.create(topoId, imageId, IOUtils.inputStreamToBytes(in));
+   private void addImageToRepository(long topoId, long imageId, int res) throws RepositoryException, IOException {
+      InputStream in = getRawStream(res);
+      imageRepository.create(topoId, imageId, IOUtils.toByteArray(in));
       in.close();
    }
 
+   private InputStream getRawStream(int id) {
+      return getInstrumentation().getContext().getResources().openRawResource(id);
+   }
+   
+   private boolean sameHeightAndWidth(Bitmap expected, Bitmap actual) {
+      return expected.getHeight() == actual.getHeight()
+         && expected.getWidth() == actual.getWidth();
+   }
+
+   private Bitmap getBitmap(int id) {
+      return BitmapFactory.decodeStream(getRawStream(id));
+   }
+
    public void testCreate() throws Exception {
-      InputStream in = getContext().getResources().openRawResource(R.raw.sure);
+      InputStream in = getInstrumentation().getContext().getResources().openRawResource(R.raw.sure);
       
-      File file = imageRepository.create(1L, 0L, IOUtils.inputStreamToBytes(in));
+      imageRepository.create(1L, 0L, IOUtils.toByteArray(in));
       in.close();
       
-      assertTrue(file.exists());
+      assertTrue(imageRepository.image(1L, 0L).exists());
+      assertNotNull(BitmapFactory.decodeFile(imageRepository.image(1L, 0L).getAbsolutePath()));
    }
    
    public void testFindByTopoId() throws Exception {
-      addImageToRepository(1L, R.raw.sure);
-      addImageToRepository(1L, R.raw.sure2);
+      addImageToRepository(1L, 0L, R.raw.sure);
+      addImageToRepository(1L, 1L, R.raw.sure2);
       
       List<Bitmap> bitmaps = imageRepository.findByTopoId(1L);
       
       assertEquals(2, bitmaps.size());
+      assertTrue(sameHeightAndWidth(bitmaps.get(0), getBitmap(R.raw.sure)));
+      assertTrue(sameHeightAndWidth(bitmaps.get(1), getBitmap(R.raw.sure2)));
    }
    
+   public void testFindByTopoIdReturnEmptyListIfNoImagesFound() throws Exception {
+      
+      List<Bitmap> bitmaps = imageRepository.findByTopoId(1L);
+
+      assertNotNull(bitmaps);
+      assertEquals(0, bitmaps.size());
+   }
+
    public void testGetReturnNullIfNotFound() throws Exception {
       
       Bitmap image = imageRepository.get(1L, R.raw.sure);
@@ -59,11 +82,12 @@ public class ImageRepositoryTest extends AndroidTestCase {
    }
    
    public void testGet() throws Exception {
-      addImageToRepository(1L, R.raw.sure);
-      addImageToRepository(1L, R.raw.sure2);
+      addImageToRepository(1L, 0L, R.raw.sure);
+      addImageToRepository(1L, 1L, R.raw.sure2);
       
-      Bitmap image = imageRepository.get(1L, R.raw.sure2);
+      Bitmap image = imageRepository.get(1L, 1L);
       
-      assertEquals(imageRepository.findByTopoId(1L).get(1), image);
+      assertNotNull(image);
+      assertTrue(sameHeightAndWidth(getBitmap(R.raw.sure2), image));
    }
 }
